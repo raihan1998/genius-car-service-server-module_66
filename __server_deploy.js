21d03d90038efc5e3db46b35e1374e8c075e4db4
unknown <you@example.com>
2022-05-14 14:34:22 +0600

    server

---
+++ __server_deploy.js
@@ -0,0 +1,27 @@
+/**
+ * ------------------------------------------
+ * ONE TIME for your computer
+ * ----------------------------------------
+ * 1. Create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * -----------------------------------------
+ * For each project one time
+ * ----------------------------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commit . git push
+ * 3. git push heroku main
+ * 4. Go to Heroku Dashboard > Current Project > setting > reveal config
+ * 5. copy paste config vars from your .env file
+ * 6. Make sure you have whitelisted all ip address to access mongodb
+ * --------------------------------------
+ * UPDATE SERVER with new changes
+ * --------------------------------------
+ * 1. Make Changes
+ * 2. make sure you: git add . git commit . git push
+ * 3. git push heroku main
+ * --------------------------------------
+ * UPDATE SERVER
+ * --------------------------------------
+*/ 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -21,15 +22,26 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
 
-    app.get("/service", async(req, res) => {
+    // auth
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "id",
+      });
+      res.send({ accessToken });
+    });
+
+    // services api
+    app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
 
-    app.get("/service/:id", async(req, res) => {
+    app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
@@ -37,17 +49,33 @@ async function run() {
     });
 
     // POST
-    app.post("/service", async(req, res) => {
+    app.post("/service", async (req, res) => {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
 
     // DELETE
-    app.delete("/service/:id", async(req, res) => {
+    app.delete("/service/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Order Collection api
+
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
   } finally {
@@ -59,6 +87,10 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running Genius Server");
 });
+
+app.get('/hero', (req, res) => {
+  res.send('Hero meets hero ku');
+})
 
 app.listen(port, () => {
   console.log("Listening to port", port);
